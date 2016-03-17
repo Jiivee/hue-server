f0f32c51fbdce8ebc43364ffc047df9f56aafc32
Jiivee <joni.vayrynen@gmail.com>
2016-03-17 16:07:03 +0200

    Change saturation and brightness as well

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,45 @@ var request = require('request');
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.send('respond with a resource');
-  console.log('hello');
+  var hue = req.body.hue;
+  var brightness = req.body.brightness;
+  var saturation = req.body.saturation;
+  var data = {
+    "on":true,
+    "sat": saturation,
+    "bri": brightness,
+    "hue": hue
+  };
+
   request({
     method: 'PUT',
-    body: '{"on":true, "sat":254, "bri":254,"hue":0}',
+    body: JSON.stringify(data),
     uri: 'http://192.168.1.7/api/GYRwirkQ698vzLpqkUe-gEu2wkWMxPZg6bjzZukI/lights/6/state'
   },
   function (error, response, body) {
-    console.log(response.body);
+    //console.log(response);
     if (!error && response.statusCode == 200) {
-      console.log(body) // Print the google web page.
+      //console.log(body); // Print the google web page.
     }
-  })
+  });
+});
+
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+
+  request({
+    method: 'GET',
+    uri: 'http://192.168.1.7/api/GYRwirkQ698vzLpqkUe-gEu2wkWMxPZg6bjzZukI/lights/6'
+  },
+  function (error, response, body) {
+    //console.log(response);
+    if (!error && response.statusCode == 200) {
+      //console.log(body); // Print the google web page.
+      res.send(body);
+    }
+  });
 });
 
 module.exports = router;

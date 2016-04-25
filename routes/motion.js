21b3979986fca28a87e57a9d07a1a59bfd754eb4
Jiivee <joni.vayrynen@gmail.com>
2016-04-25 11:38:10 +0300

    New motion sensor stuff

--- routes/motion.js
+++ routes/motion.js
@@ -2,35 +2,84 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var lastMovement;
+var movement;
+var lightOn = false;
+var timeNow;
 
-/* GET users listing. */
+
+//These can be changed by the user
+var lightTime = 10000;
+var motionStatus = true;
+
+var dataOn = {
+  "on": true
+};
+
+var dataOff = {
+  "on": false
+};
+
+/* Get motion status */
 router.post('/', function(req, res, next) {
   res.send('Motion message received');
-  console.log(req.body);
   var on = parseInt(req.body.on);
-  var data;
   if (on === 1) {
-    data = {
-      "on": true
-    };
+    movement = true;
+    if (lightOn === false) {
+      sendData(dataOn);
+      lightOn = true;
+    }
   }
   else {
-    data = {
-      "on": false
-    };
+    lastMovement = Date.now();
+    movement = false;
   }
-
-  request({
-    method: 'PUT',
-    body: JSON.stringify(data),
-    uri: 'http://192.168.1.7/api/GYRwirkQ698vzLpqkUe-gEu2wkWMxPZg6bjzZukI/lights/6/state'
-  },
-  function (error, response, body) {
-    //console.log(response);
-    if (!error && response.statusCode == 200) {
-      //console.log(body); // Print the google web page.
-    }
-  });
 });
+
+
+/* Get motion interval */
+router.post('/lighttime', function(req, res, next) {
+  res.send('Motion message received');
+  lightTime = parseInt(req.body.lighttime);
+
+});
+
+/* Get motion on (true) or off (false) */
+router.post('/status', function(req, res, next) {
+  res.send('Motion message received');
+  motionStatus = req.body.motionStatus;
+  console.log(motionStatus);
+  console.log(motionStatus === true);
+});
+
+
+var timer = setInterval(function() {
+  console.log(lightTime);
+  timeNow = Date.now();
+  if (timeNow - lastMovement > lightTime && movement === false) {
+    sendData(dataOff);
+    lightOn = false;
+  }
+}, 2000);
+
+
+function sendData(data) {
+  if (motionStatus === true) {
+    data = JSON.stringify(data);
+    request({
+      method: 'PUT',
+      body: data,
+      uri: 'http://192.168.1.7/api/GYRwirkQ698vzLpqkUe-gEu2wkWMxPZg6bjzZukI/lights/6/state'
+    },
+    function (error, response, body) {
+      //console.log(response);
+      if (!error && response.statusCode == 200) {
+        //console.log(body); // Print the google web page.
+        console.log();
+      }
+    });
+  }
+}
 
 module.exports = router;

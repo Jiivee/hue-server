9db6f44009ea1e9ec133cbaa89a715e79dc0b753
Teemu Leivo <teemu.leivo@gofore.com>
2016-05-04 17:25:21 +0300

    deleted index.js and modified app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var lights = require('./routes/lights');
 var motion = require('./routes/motion');
 
@@ -26,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/lights', lights);
 app.use('/motion', motion);
 
